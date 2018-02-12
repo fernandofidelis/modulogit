c9b4494b3ae75b2388b7f67fb1c9ffd3ee080b97
Fidelis <fidelis_f@hotmail.com>
2018-02-12 18:16:10 -0200

    Branch de teste

--- HTML/JS/script 20.js
+++
@@ -1,9 +0,0 @@
-function pressTecla(eventos){
-	console.log("tecla shift "+ String.fromCharCode(eventos.charCode));
-	console.log("tecla shift "+ eventos.charCode);
-
-	/*if (eventos.shiftKey == true && eventos.keyCode == 69) {
-		console.log("tecla shift "+ eventos.keyCode);
-	} 
-*/
-}
